93943bbe9033ceb141a02a0f486c65b0f152d745
Aldair89 <114510628+Aldair89@users.noreply.github.com>
2022-11-08 18:32:46 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function dispararAlerta () {
+alert("Hola desde javaScript")
+}
+function dispararSuma () {
+  let n1=10
+  let n2=10
+alert(n1+n2)
+}
+function disparardivicion(){
+  let n2=10
+  let n3=32
+alert(n2/n3)
+}
